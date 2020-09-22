30cb45ff6052a56b4a91808c27f3419c0566d302
Lucas <lucas98fernando@gmail.com>
2020-09-21 22:14:49 -0300

    Creating a new component and adding your route

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 // Importando as views de Home e Product-Crud
 import { HomeComponent } from './views/home/home.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
+// Importando o componente de criação de produtos
+import { ProductCreateComponent } from './components/products/product-create/product-create.component';
 
 // Definição das rotas de navegação
 const routes: Routes = [
@@ -14,6 +16,10 @@ const routes: Routes = [
   {
     path: "products",
     component: ProductCrudComponent
+  },
+  {
+    path: "products/create",
+    component: ProductCreateComponent
   }
 ];
 
